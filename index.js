4123f620a158d1be03fbc8b76af35200e2077844
SamFio00 <samuele.fiorini2000fr@gmail.com>
2026-05-06 12:19:00 +0200

    initial setup w Express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.json({ message: "All is working fine" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
