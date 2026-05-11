462d543fcea7c9b812957b21f2862cfd06387086
SamFio00 <samuele.fiorini2000fr@gmail.com>
2026-05-11 13:40:00 +0200

    changes swaps

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        surname: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+            unique: true
+        },
+    },
+
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
